d3066ffb6945ad614eb15859b19092b652022960
rajat <thisisrs778@gmail.com>
2020-05-03 14:56:41 +0530

    Flutter music player

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,67 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void playSound(int a){
+    final player = AudioCache();
+    player.play('note$a.wav');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            children: <Widget>[
+              FlatButton(
+                onPressed: (){
+                  playSound(1);
+                },
+               color: Colors.red,
+              ),
+              FlatButton(
+                onPressed: (){
+                  playSound(2);
+                },
+                color: Colors.orange,
+              ),
+              FlatButton(
+                onPressed: (){
+                  playSound(3);
+                },
+                color: Colors.yellow,
+              ),
+              FlatButton(
+                onPressed: (){
+                  playSound(4);
+                },
+                color: Colors.green,
+              ),
+              FlatButton(
+                onPressed: (){
+                  playSound(5);
+                },
+                color: Colors.lightGreen,
+              ),
+              FlatButton(
+                onPressed: (){
+                  playSound(6);
+                },
+                color: Colors.blue,
+              ),
+              FlatButton(
+                onPressed: (){
+                  playSound(7);
+                },
+                color: Colors.purple,
+              ),
+            ],
+
+      ),
         ),
       ),
     );
